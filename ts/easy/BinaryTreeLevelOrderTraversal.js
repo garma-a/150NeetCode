5b57348f23954e6f78a3c27dd956f4c0334874d8
Girgis <girgisemad74@gmail.com>
2026-01-08 22:42:42 +0200

    solve two medium

---
+++ ts/easy/BinaryTreeLevelOrderTraversal.js
@@ -0,0 +1,35 @@
+class TreeNode {
+	constructor(val = 0, left = null, right = null) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+
+class Solution {
+	/**
+	 * @param {TreeNode} root
+	 * @return {number[][]}
+	 */
+	levelOrder(root) {
+		if (!root) return [];
+		const q = [root];
+		const ans = [];
+		while (q.length) {
+			const level = [];
+			const copy = [...q];
+			for (const node of copy) {
+				level.push(node.val);
+				q.shift();
+				if (node.left)
+					q.push(node.left);
+				if (node.right)
+					q.push(node.right);
+			}
+			ans.push([...level]);
+
+		}
+		return ans;
+	}
+}
