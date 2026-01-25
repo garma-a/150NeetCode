4202b4e36ee26105e79f5b2391615f465fb23c87
Girgis <girgisemad74@gmail.com>
2026-01-25 21:33:37 +0200

    solve two problems

---
+++ py/easy/MeetingRooms.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:return True
+        sorted_intervals = sorted(intervals, key=lambda interval: interval.start)  
+        prev_end =  sorted_intervals[0].end
+
+        for index in range(1,len(sorted_intervals)):
+            start , end= sorted_intervals[index].start , sorted_intervals[index].end
+            if prev_end<=start:
+                prev_end = end
+            else: return False
+
+        return True
