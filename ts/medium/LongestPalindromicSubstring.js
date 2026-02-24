a0b755c11001fe1fc90dee9da0fcab794f166469
Girgis <girgisemad74@gmail.com>
2026-02-24 16:59:47 +0200

    solve one medium problem

--- ts/medium/LongestPalindromicSubstring.js
+++ ts/medium/LongestPalindromicSubstring.js
@@ -4,29 +4,28 @@ class Solution {
    * @return {string}
    */
   longestPalindrome(s) {
-    if (s.length < 2) return s;
-    let start = 0, maxLen = 0, ans = "";
+
     function expandAroundCenter(left, right) {
-      while (left >= 0 && right < s.length && s[left] === s[right]) {
+      while (left >= 0 && right < s.length) {
+        if (s[left] !== s[right])
+          break;
         left--;
         right++;
       }
-      return [right - left - 1, left + 1];
+      return [left + 1, right - left - 1];
     }
+    let ans = "";
     for (let i = 0; i < s.length; i++) {
-      const [oddLength, startOdd] = expandAroundCenter(i, i);
-      const [evenLength, startEven] = expandAroundCenter(i, i + 1);
-      if (maxLen < oddLength) {
-        ans = s.substring(startOdd, startOdd + oddLength);
-        maxLen = oddLength
-      }
-      if (maxLen < evenLength) {
-        ans = s.substring(startEven, startEven + evenLength);
-        maxLen = evenLength
-      }
+      let [oddStart, oddLength] = expandAroundCenter(i, i);
+      let [evenStart, evenLength] = expandAroundCenter(i, i + 1);
+      if (evenLength > ans.length)
+        ans = s.substring(evenStart, evenLength + evenStart);
+      if (oddLength > ans.length)
+        ans = s.substring(oddStart, oddLength + oddStart);
 
     }
     return ans;
+
   }
 }
 
