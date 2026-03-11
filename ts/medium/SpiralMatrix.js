4ff26aff93b8a9020e5d62ef5374297ee9039dd4
Girgis <girgisemad74@gmail.com>
2026-03-11 12:24:48 +0200

    solved one probelm

--- ts/medium/SpiralMatrix.js
+++ ts/medium/SpiralMatrix.js
@@ -1,73 +1,35 @@
-
 class Solution {
   /**
    * @param {number[][]} matrix
    * @return {number[]}
    */
   spiralOrder(matrix) {
-
-    const ROW = matrix.length, COL = matrix[0].length;
-    let lt = [0, 0], rt = [0, COL - 1], rb = [ROW - 1, COL - 1], lb = [ROW - 1, 0];
-    let res = [];
-
-    while (lt[0] <= rb[0] && lt[1] <= rb[1]) {
-      if (lt[0] === rb[0] && lt[1] === rb[1]) {
-        res.push(matrix[lt[0]][lt[1]]);
-        break;
-      } else if (lt[0] === rb[0]) {
-        this.fromTo(lt, rt, matrix, res);
-        break
-      } else if (lt[1] === rb[1]) {
-        this.fromTo(lt, lb, matrix, res);
-        break;
-      } else {
-        this.fromTo(lt, rt, matrix, res);
-        this.fromTo([rt[0] + 1, rt[1]], rb, matrix, res);
-        this.fromTo([rb[0], rb[1] - 1], lb, matrix, res);
-
-        if (lb[0] - 1 >= lt[0] + 1)
-          this.fromTo([lb[0] - 1, lb[1]], [lt[0] + 1, lt[1]], matrix, res);
+    let ROW = matrix.length, COL = matrix[0].length;
+    let L = 0, R = COL - 1, T = 0, B = ROW - 1;
+    const res = [];
+    while (L <= R && T <= B) {
+      for (let start = L; start <= R; start++) {
+        res.push(matrix[T][start]);
       }
-      lt = [lt[0] + 1, lt[1] + 1];
-      rt = [rt[0] + 1, rt[1] - 1];
-      rb = [rb[0] - 1, rb[1] - 1];
-      lb = [lb[0] - 1, lb[1] + 1];
+      T += 1;
+      for (let start = T; start <= B; start++) {
+        res.push(matrix[start][R]);
+      }
+      R -= 1;
+      if (T <= B) {
+        for (let start = R; start >= L; start--) {
+          res.push(matrix[B][start]);
+        }
+        B -= 1;
+      }
+      if (L <= R) {
+        for (let start = B; start >= T; start--) {
+          res.push(matrix[start][L]);
+        }
+        L += 1;
+      }
     }
     return res;
-
   }
-
-
-  fromTo(from, to, matrix = [], res = []) {
-    const [fromRow, fromCol] = from, [toRow, toCol] = to;
-    let rowIsFixed = false, ourRow = 0;
-    if (fromRow === toRow) {
-      rowIsFixed = true;
-      ourRow = fromRow;
-    }
-    let colIsFixed = false, ourCol = 0;
-    if (fromCol === toCol) {
-      colIsFixed = true;
-      ourCol = fromCol;
-    }
-
-    if (rowIsFixed && colIsFixed) res.push(matrix[ourRow][ourCol]);
-
-    else if (rowIsFixed && fromCol < toCol)
-      for (let c = fromCol; c <= toCol; c++) res.push(matrix[ourRow][c]);
-
-    else if (rowIsFixed && fromCol > toCol)
-      for (let c = fromCol; c >= toCol; c--) res.push(matrix[ourRow][c]);
-
-    else if (colIsFixed && fromRow < toRow)
-      for (let r = fromRow; r <= toRow; r++) res.push(matrix[r][ourCol]);
-
-    else if (colIsFixed && fromRow > toRow)
-      for (let r = fromRow; r >= toRow; r--) res.push(matrix[r][ourCol]);
-
-
-  }
-
-
-
 }
+
